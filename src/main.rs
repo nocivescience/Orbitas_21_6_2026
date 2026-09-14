2acabfe026b4ee90d4b501c2e04ba768bb8e2a5a
nocivescience <nocive.science@gmail.com>
2026-09-14 18:17:43 -0300

    puntos verdes agregados

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+use macroquad::prelude::*;
+
+
 trait Orbitar {
     fn describir_orbita(&self);
     fn avanzar_orbita(&mut self, dias: &u32);
@@ -54,8 +57,10 @@ fn simular_dia_en_el_cosmos(cuerpo: &mut impl Orbitar,dias_a_pasar: &u32) {
     cuerpo.avanzar_orbita(dias_a_pasar);
     cuerpo.describir_orbita();
 }
-
-fn main() {
+#[macroquad::main("Hello Macroquad")]
+async fn main() {
+    let mut puntos = Vec::new();
+    
     let mut tierra = Planeta{
         nombre: String::from("Tierra"),
         periodo_orbital: 365.25,
@@ -72,6 +77,7 @@ fn main() {
         dias_transcurridos: 70,
     };
     let tiempo_simulacion = 90;
+    let texto = "Simulacion de Orbita";
     println!("-------- Iniciando la Simulacion -----------");
     simular_dia_en_el_cosmos(&mut tierra, &tiempo_simulacion);
     let factor_velocidad = 2;
@@ -83,4 +89,19 @@ fn main() {
     println!("\nLuminocidad ultra roja: {} lum", factor_luminario);
     simular_dia_en_el_cosmos(&mut estrella, &factor_luminario);
     simular_dia_en_el_cosmos(&mut cometa, &factor_transito);
+    loop{
+        if is_mouse_button_pressed(MouseButton::Left){
+            let mouse_position = mouse_position();
+            puntos.push(mouse_position);    
+        };
+        clear_background(BLACK);
+        draw_text(&texto, 200.0, 200.0, 30.0, RED);
+        draw_text(&format!("Dias transcurridos en la Tierra: {}", tierra.dias_transcurridos), 200.0, 250.0, 20.0, WHITE);
+        draw_text(&format!("Dias transcurridos en el Cometa: {}", cometa.dias_transcurridos), 200.0, 300.0, 20.0, WHITE);
+        draw_text(&format!("Edad de la Estrella: {} años", estrella.edad), 200.0, 350.0, 20.0, WHITE);
+        for punto in &puntos {
+            draw_circle(punto.0, punto.1, 5.0, GREEN);
+        }
+        next_frame().await;
+    }
 }
